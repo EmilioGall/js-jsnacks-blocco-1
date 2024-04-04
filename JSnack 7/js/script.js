128e7e282cecd5c10a8b7b6a8446c430c1e141ac
Emilio Gallo <emilio.gallo17@gmail.com>
2024-04-04 23:33:56 +0200

    add: content6style - JSnack7 - html&js

--- JSnack 7/js/script.js
+++ JSnack 7/js/script.js
@@ -1,39 +1,8 @@
-// TEXT
-// Il software deve chiedere per 10 volte all'utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
+// TEXT JSnack7
+// Stampa le potenze di 2 fino a 1000.
 
-const sumVal = document.getElementById("sum"); // object | null
+for ( i = 0; i < 1000; i++) {
 
-console.log(sumVal, typeof sumVal);
+    console.log("2^", i, "=", Math.pow(2, i));
 
-let sumInputs = 0;
-
-let numArray = [];
-
-const nNum = 10;
-
-let i = 0;
-
-while (i < nNum) {
-
-    const inputNum = parseInt(prompt(`Hello, User! Insert ${nNum - i} numers.`));
-
-    console.log(inputNum, i+1, typeof inputNum);
-
-    if (!isNaN(inputNum)) {
-
-        sumInputs += inputNum;
-
-        console.log(sumInputs, typeof sumInputs);
-
-        i++;
-
-    } else {
-
-        alert("You must enter a number!");
-
-    }
-
-}
-
-sumVal.innerHTML = sumInputs;
+};
